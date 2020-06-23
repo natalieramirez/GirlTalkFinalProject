614b54b94b97cefeedfcce0178893578a073aa61
siegelje <62024595+siegelje@users.noreply.github.com>
2020-06-23 14:56:13 -0400

    Add files via upload

---
+++ GirlTalk/GirlTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GirlTalk
+//
+//  Created by Jemma Siegel on 6/23/20.
+//  Copyright © 2020 Jemma Siegel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
